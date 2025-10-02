f4a230f1847c4bf26a406ab694dd4f53816e56ab
glindy89 <35814731+glindy89@users.noreply.github.com>
2025-10-02 11:50:06 -0700

    Update CatalogGear.jsx

--- src/components/CatalogGear.jsx
+++ src/components/CatalogGear.jsx
@@ -21,7 +21,7 @@ class CatalogGear extends Component {
                 <a href="#/gearproducts">
                   <figure className="image is-16by9">
                     <img
-                      src="https://teehuggers.com/aca/aca_tech_1.jpeg"
+                      src="https://teehuggers.com/aca/aca_tech_2.jpeg"
                       alt="Gear Watches"
                       style={{ width: "100%" }}
                     />
@@ -44,7 +44,7 @@ class CatalogGear extends Component {
                 <a href="#/gearproducts">
                   <figure className="image is-16by9">
                     <img
-                      src="https://teehuggers.com/aca/aca_tech_1.jpeg"
+                      src="https://teehuggers.com/aca/aca_tech_3.jpeg"
                       alt="Gear Hydration"
                       style={{ width: "100%" }}
                     />
@@ -67,7 +67,7 @@ class CatalogGear extends Component {
                 <a href="#/gearproducts">
                   <figure className="image is-16by9">
                     <img
-                      src="https://teehuggers.com/aca/aca_tech_1.jpeg"
+                      src="https://teehuggers.com/aca/aca_tech_4.jpeg"
                       alt="Gear Sunglasses"
                       style={{ width: "100%" }}
                     />
